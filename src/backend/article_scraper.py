9ca681d6b760dcad7e8052b1f6206555b2f31ae1
Gabriel_Freeze <gabriel.hili.20@um.edu.mt>
2024-04-12 17:07:20 +0200

    bruh

---
+++ src/backend/article_scraper.py
@@ -0,0 +1,110 @@
+import re
+import requests
+import traceback
+from lxml import html
+from base64 import b64encode
+from selenium import webdriver
+from lxml.html import HtmlElement
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from utils import color, get_img_ext, Payload
+
+class ArticleScraper:
+    def __init__(self):
+        self.headers = {'User-Agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.6312.86 Safari/537.36"}
+        
+        #Initialise Selenium webdriver in cases were JS is required to fully load page.
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--log-level=3")
+        self.driver = webdriver.Chrome(options=chrome_options)
+        
+    def scrape(self, url:str) -> Payload:
+        scraper_map = {"timesofmalta":self.scrape_tom,
+                       "theshiftnews":self.scrape_ts}
+        match = re.search(r"(?:https?:\/\/)(?:www\.)?([a-zA-Z0-9-]+)\.com", url)
+
+        #Ensure that the provided website is valid
+        if match and (key:=match.group(1)) in scraper_map.keys():
+            return scraper_map[key](url)
+        else:
+            return Payload(error=f"{url} does not belong to the following domain names: {list(scraper_map.keys())}")
+        
+    def scrape_tom(self,url:str):        
+        
+        if (k:="timesofmalta.com/article/") not in url:
+            return Payload(error=f"URL does not include sub-string {k}")
+        
+        try:
+            #Get article
+            content = requests.get(url,headers=self.headers).content.decode('utf-8')
+        
+            tree = html.fromstring(content)          
+
+            #Get Title
+            title = tree.xpath('/html/head/title')[0].text
+
+            #Get links to Thumbnail + Images
+            
+            self.driver.get(url) #ToM websites are rendered using JavaScript so we have to use Selenium.
+            img_links = self.driver.find_elements(By.XPATH,'//*[@id="article-head"]/div/picture/img') + \
+                        self.driver.find_elements(By.XPATH,'//*[@id="observer"]/main/article/div[2]/div/*/img')
+            
+            #Save byte data of images to list
+            imgs = [{"img": {"data":b64encode(requests.get(k:=img.get_attribute('src')).content).decode("ascii"),
+                             "file":get_img_ext(k)},
+                     "alt": img.get_attribute('alt')}
+                    for img in img_links]
+                    
+            #Get Body
+            body = self.get_nested_text(tree.xpath('/html/body/div/main/article/div[2]/div')[0])
+
+        except Exception as e:
+            traceback.print_exc()
+            return Payload(error=f"Unexpected Error: {traceback.format_exc()}")
+
+        return Payload(data={"title":title, "imgs":imgs, "body" :body})
+
+    def scrape_ts(self,url:str):
+        
+        if not re.search(r"(?:https?:\/\/)(?:www\.)?theshiftnews\.com\/[0-9]{4}\/[0-9]{2}\/[0-9]{2}\/",url):
+            return Payload(error=f"URL is not a valid TheShiftNews article")
+        
+        try:
+            content = requests.get(url,headers=self.headers).content.decode('utf-8')
+            
+            with open("test.html","w", encoding="utf-8") as f:
+                f.write(content)
+            
+            tree = html.fromstring(content)
+            
+            #Skip Maltese Articles
+            if tree.xpath('//*[@id="container"]/div[4]/p[1]/em'):
+                return {}
+
+            title = tree.xpath('//*[@id="container"]/h2')[0].text
+
+            #Get Body
+            body = ' '.join([b.text for b in tree.xpath('//*[@id="container"]/div[4]/p') if type(b.text) == str])
+
+            #Get Images + Captions
+            img_links = tree.xpath('//*[@class="featured_image"]')[:1] + \
+                        tree.xpath('//*[@class="wp-caption alignnone"]')
+
+            #Save byte data of images to list
+            imgs = [{"img": {"data":b64encode(requests.get(k:=img.xpath('./img')[0].attrib['src']).content),
+                             "file":get_img_ext(k)},
+                    "alt": img.xpath('./p')[0].text}
+                    for img in img_links]
+        
+        except Exception as e:
+            traceback.print_exc()
+            return Payload(f"Unexpected Error: {traceback.format_exc()}")
+        
+        return Payload(data={"title":title, "imgs":imgs, "body" :body})
+
+    def get_nested_text(self,element:HtmlElement): 
+        return "".join([(child.text or "") + self.get_nested_text(child) + (child.tail or "")
+                        for child in element.iterchildren()
+                        if child.tag in ['p','strong','i','b','u','em','a']])
+       
---
+++ src/backend/main.py
@@ -0,0 +1,93 @@
+import asyncio
+from time import time
+from random import randint
+import multiprocessing
+from fastapi import FastAPI
+from gpu_backend import GPU_Backend
+from utils import Payload, GPU_Payload, color
+from article_scraper import ArticleScraper
+from fastapi.middleware.cors import CORSMiddleware
+    
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
+)
+
+
+artScraper = ArticleScraper()
+init:bool = False
+job_no:int = 0
+
+queue_1: multiprocessing.Queue = None
+queue_2: multiprocessing.Queue = None
+        
+
+ #TODO: Multiple requests are not being handled
+@app.get("/")
+async def endpoint(url:str=""):
+    global init, job_no, artScraper, queue_1, queue_2
+    
+    if not init:
+        #Queues for maintaining communication between this process and `gpu_proc`
+        queue_1 = multiprocessing.Queue()
+        queue_2 = multiprocessing.Queue()
+
+        #Initialise GPU backend
+        job = multiprocessing.Process(target=gpu_proc, args=(queue_1,queue_2))
+        job.start()
+        
+        #Initialisaion ready
+        init=True
+      
+    this_job_no = job_no
+    job_no = (job_no+1)%256 #Handles theoretical edge case where integer reaches limit
+    
+    print(f'[{this_job_no}] Start')
+    # sleep(randint(0,10))
+    all_time = time()
+    
+    #Extract data from URL to pass to `gpu_proc`
+    s=time()
+    payload:Payload = artScraper.scrape(url)
+    print(f'[{this_job_no}] Scraping finished: {color.GREEN}{round(time()-s,2)}s{color.ESC}')
+
+    
+    #Don't invoke job if error in scraping
+    if payload.error:
+        return payload
+    
+    #Place data in queue_1 to be read by `gpu_proc`
+    #Very important that I use kwargs due to `__init__` overloading.
+    queue_1.put(GPU_Payload(job_no=this_job_no,
+                            payload=payload))
+    
+    #Await the return payload from `gpu_proc`
+    #Make sure the output we retrieved is the one produced by our payload.
+    while (return_payload:=queue_2.get()).job_no != this_job_no and not return_payload.error:
+        print(return_payload.error)
+        queue_2.put(return_payload) #Place output back in queue_2 for correct process to consume
+    
+    print(f'[{this_job_no}] Finished in {round(time()-all_time,2)}s')    
+    
+    
+    #`job_no` is not included in the dict
+    return return_payload.to_dict()
+    
+    
+    
+#Spin new thread and send data to GPU when received
+def gpu_proc(queue_1:multiprocessing.Queue,
+             queue_2:multiprocessing.Queue):
+    s=time()
+    gpu_backend = GPU_Backend()
+    print(f"Initialising GPU Backend: [{round(time()-s,2)}s]")
+    
+    while True:
+        gpu_payload = queue_1.get() #Blocks on empty queue
+        queue_2.put(
+            gpu_backend(gpu_payload)
+        )
